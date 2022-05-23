d726e6de8bb97e224711082851c63692374082ee
Chris Wu <chris@faros.ai>
2022-05-23 09:56:55 -0700

    Write airbyte files to tmp folder and delete it afterwards

--- airbyte-local.sh
+++ airbyte-local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+echo -e "Using bash version ${BASH_VERSION}. This script requires at least bash version 4.0\n"
 
 declare -a required_cmds=("docker" "jq")
 for i in "${required_cmds[@]}"; do
@@ -120,18 +120,18 @@ function writeSrcConfig() {
       split("\u0000")
       | . as $a
       | reduce range(0; length/2) as $i
-          ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})' > $src_config_filename
+          ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})' > "$tempdir/$src_config_filename"
 }
 
 # TODO: take catalog as input parameter
 function writeSrcCatalog() {
     case $src_docker_image in
         *pagerduty*)
-            echo $pagerduty_src_catalog | jq > $src_catalog_filename
+            echo $pagerduty_src_catalog | jq > $tempdir/$src_catalog_filename
             stream_prefix=mypagerdutysrc__pagerduty__
             ;;
         *servicenow*)
-            echo $servicenow_src_catalog | jq > $src_catalog_filename
+            echo $servicenow_src_catalog | jq > $tempdir/$src_catalog_filename
             stream_prefix=myservicenowsrc__servicenow__
             ;;
         *)
@@ -148,23 +148,33 @@ function writeDstConfig() {
         --arg api_url "${dst_config[faros_api_url]}" \
         --arg api_key "${dst_config[faros_api_key]}" \
         --arg graph "${dst_config[graph]}" \
-        '{edition_configs: {edition: "cloud", api_url: $api_url, api_key: $api_key, graph: $graph}}' > $dst_config_filename
+        '{edition_configs: {edition: "cloud", api_url: $api_url, api_key: $api_key, graph: $graph}}' > $tempdir/$dst_config_filename
 }
 
 function writeDstCatalog() {
-    cat $src_catalog_filename | jq ".streams[].stream.name |= \"${stream_prefix}\" + ." > $dst_catalog_filename
+    cat $src_catalog_filename | jq ".streams[].stream.name |= \"${stream_prefix}\" + ." > $tempdir/$dst_catalog_filename
 }
 
 function sync() {
     echo -e "\nRunning source image and piping output to destination image. Destination logs will be shown below."
-    docker run --rm -v $(pwd):/configs $src_docker_image read --config /configs/$src_config_filename --catalog /configs/$src_catalog_filename | \
+    docker run --rm -v "$tempdir:/configs" "$src_docker_image" read --config "/configs/$src_config_filename" --catalog "/configs/$src_catalog_filename" | \
     jq -c -R $jq_cmd "fromjson? | select(.type == \"RECORD\") | .record.stream |= \"${stream_prefix}\" + ." | \
-    docker run -i -v $(pwd):/configs $dst_docker_image write --config /configs/$dst_config_filename --catalog /configs/$dst_catalog_filename
-
+    docker run -i -v "$tempdir:/configs" "$dst_docker_image" write --config "/configs/$dst_config_filename" --catalog "/configs/$dst_catalog_filename"
 }
 
+tempdir=$(mktemp -d)
+echo "Created folder $tempdir for temporary airbyte files"
+
+function cleanup() {
+    rm -rf "$tempdir"
+}
+
+trap cleanup EXIT
+trap cleanup SIGINT
+
+set -eo pipefail
+
 main() {
-    echo -e "Using bash version ${BASH_VERSION}. This script requires at least bash version 4.0\n"
     setDefaults
     parseFlags "$@"
     writeSrcConfig
