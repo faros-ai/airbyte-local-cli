1dbb2c98361a264eef522bfe178770bc9378059c
Chris Wu <chris@faros.ai>
2022-08-03 20:49:49 -0700

    Allow users to specify their own catalogs

--- airbyte-local.sh
+++ airbyte-local.sh
@@ -20,6 +20,12 @@ src_catalog_filename=${filename_prefix}_src_catalog.json
 dst_config_filename=${filename_prefix}_dst_config.json
 dst_catalog_filename=${filename_prefix}_dst_catalog.json
 
+# Theme
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 function setDefaults() {
     declare -Ag src_config=()
     declare -Ag dst_config=( ["graph"]="default" )
@@ -40,8 +46,14 @@ function parseFlags() {
             --state)
                 src_state_filepath="$2"
                 shift 2 ;;
-            --src.catalog)
+            --src-catalog)
                 src_catalog_overrides="$2"
+                shift 2 ;;
+            --src-catalog-file)
+                src_catalog_file="$2"
+                shift 2 ;;
+            --src-catalog-json)
+                src_catalog_json="$2"
                 shift 2 ;;
             --src.*)
                 IFS='.' read -ra strarr <<< $1
@@ -76,9 +88,18 @@ function parseFlags() {
             --dst-stream-prefix)
                 dst_stream_prefix="$2"
                 shift 2 ;;
+            --dst-catalog-file)
+                dst_catalog_file="$2"
+                shift 2 ;;
+            --dst-catalog-json)
+                dst_catalog_json="$2"
+                shift 2 ;;
             --max-log-size)
                 max_log_size="$2"
                 shift 2 ;;
+            --debug)
+                debug=true
+                shift 1 ;;
             *)
                 POSITION+=("$1")
                 shift ;;
@@ -104,21 +125,33 @@ function discoverSrc() {
 }
 
 function writeSrcCatalog() {
-    discoverSrc | \
-        tee >(jq -c -R $jq_cmd "fromjson? | select(.type != \"CATALOG\")" 1>&2) | \
-        jq --arg full_refresh "$full_refresh" \
-           --argjson src_catalog_overrides "$src_catalog_overrides" '{
-          streams: [
-            .catalog.streams[]
-              | select($src_catalog_overrides[.name] != "disabled")
-              | .incremental = ((.supported_sync_modes|contains(["incremental"])) and ($src_catalog_overrides[.name] != "full_refresh") and ($full_refresh != "true"))
-              | {
-                  stream: {name: .name},
-                  sync_mode: (if .incremental then "incremental" else "full_refresh" end),
-                  destination_sync_mode: (if .incremental then "append" else "overwrite" end)
-                }
-          ]
-        }' > $tempdir/$src_catalog_filename
+    if [[ "$src_catalog_file" ]]; then
+        if [[ -s "$src_catalog_file" ]]; then
+            cp "$src_catalog_file" "$tempdir/$src_catalog_filename"
+        else
+            err "Source catalog file $src_catalog_file doesn't exist"
+            fail
+        fi
+    elif [[ "$src_catalog_json" ]]; then
+        echo "$src_catalog_json" > "$tempdir/$src_catalog_filename"
+    else
+        discoverSrc | \
+            tee >(jq -c -R $jq_cmd "fromjson? | select(.type != \"CATALOG\")" 1>&2) | \
+            jq --arg full_refresh "$full_refresh" \
+               --argjson src_catalog_overrides "$src_catalog_overrides" '{
+              streams: [
+                .catalog.streams[]
+                  | select($src_catalog_overrides[.name] != "disabled")
+                  | .incremental = ((.supported_sync_modes|contains(["incremental"])) and ($src_catalog_overrides[.name] != "full_refresh") and ($full_refresh != "true"))
+                  | {
+                      stream: {name: .name},
+                      sync_mode: (if .incremental then "incremental" else "full_refresh" end),
+                      destination_sync_mode: (if .incremental then "append" else "overwrite" end)
+                    }
+              ]
+            }' > $tempdir/$src_catalog_filename
+    fi
+    debug "Using source configured catalog: $(jq -c < $tempdir/$src_catalog_filename)"
 }
 
 function parseStreamPrefix() {
@@ -132,9 +165,9 @@ function parseStreamPrefix() {
             [ -z "$connection_name" ] && connection_name="my$(IFS= ; echo "${src_docker_image_parts[*]}")src"
             src_type=$(IFS=_ ; echo "${src_docker_image_parts[*]}")
             dst_stream_prefix="${connection_name}__${src_type}__"
-        elif [ -z "$dst_stream_prefix" ]; then
-            echo "Error: $dst_docker_image requires a destination stream prefix. Specify this by adding '--dst-stream-prefix <value>'"
-            exit 1
+        elif [[ -z "$dst_stream_prefix" ]]; then
+            err "$dst_docker_image requires a destination stream prefix. Specify this by adding '--dst-stream-prefix <value>'"
+            fail
         fi
     fi
 }
@@ -167,13 +200,25 @@ EOF
 }
 
 function writeDstCatalog() {
-    cat "$tempdir/$src_catalog_filename" | jq ".streams[].stream.name |= \"${dst_stream_prefix}\" + ." > "$tempdir/$dst_catalog_filename"
+    if [[ "$dst_catalog_file" ]]; then
+        if [[ -s "$dst_catalog_file" ]]; then
+            cp "$dst_catalog_file" "$tempdir/$dst_catalog_filename"
+        else
+            err "Destination catalog file $dst_catalog_file doesn't exist"
+            fail
+        fi
+    elif [[ "$dst_catalog_json" ]]; then
+        echo "$dst_catalog_json" > "$tempdir/$dst_catalog_filename"
+    else
+        cat "$tempdir/$src_catalog_filename" | jq ".streams[].stream.name |= \"${dst_stream_prefix}\" + ." > "$tempdir/$dst_catalog_filename"
+    fi
+    debug "Using destination configured catalog: $(jq -c < $tempdir/$dst_catalog_filename)"
 }
 
 function loadState() {
     [ -z "$src_state_filepath" ] && src_state_filepath="${connection_name}__state.json"
-    echo "Using state file: $src_state_filepath"
-    if [ -s "$src_state_filepath" ]; then
+    log "Using state file: $src_state_filepath"
+    if [[ -s "$src_state_filepath" ]]; then
         cat "$src_state_filepath" > "$tempdir/$src_state_filename"
     else
         echo "{}" > "$tempdir/$src_state_filename"
@@ -200,15 +245,21 @@ function readSrc() {
 }
 
 function checkSrc() {
-    if [ "$check_src_connection" = true ]; then
-        echo "Validating connection to source..."
+    if [[ "$check_src_connection" = true ]]; then
+        log "Validating connection to source..."
         connectionStatusInfo=$(docker run --rm -v "$tempdir:/configs" "$src_docker_image" check --config "/configs/$src_config_filename")
         connectionStatus=$(echo "$connectionStatusInfo" | jq -r '.connectionStatus.status')
-        if [ "$connectionStatus" != 'SUCCEEDED' ]; then
-            echo $(echo "$connectionStatusInfo" | jq -r '.connectionStatus.message')
-            exit 1;
+        if [[ "$connectionStatus" != 'SUCCEEDED' ]]; then
+            err $(echo "$connectionStatusInfo" | jq -r '.connectionStatus.message')
+            fail
         fi
-        echo "Connection validation successful"
+        log "Connection validation successful"
+    fi
+}
+
+function debug() {
+    if [[ "$debug" = true ]]; then
+        log "$*"
     fi
 }
 
@@ -227,19 +278,19 @@ set -eo pipefail
 main() {
     setDefaults
     parseFlags "$@"
-    if [ "$no_src_pull" = true ]; then
-        echo "Skipping pull of source image $src_docker_image"
+    if [[ "$no_src_pull" = true ]]; then
+        log "Skipping pull of source image $src_docker_image"
     else
-        echo "Pulling source image $src_docker_image"
+        log "Pulling source image $src_docker_image"
         docker pull $src_docker_image
     fi
     writeSrcConfig
     writeSrcCatalog
     parseStreamPrefix
-    if [ "$no_dst_pull" = true ]; then
-        echo "Skipping pull of destination image $dst_docker_image"
+    if [[ "$no_dst_pull" = true ]]; then
+        log "Skipping pull of destination image $dst_docker_image"
     else
-        echo "Pulling destination image $dst_docker_image"
+        log "Pulling destination image $dst_docker_image"
         docker pull $dst_docker_image
     fi
     writeDstConfig
@@ -247,13 +298,67 @@ main() {
     checkSrc
     loadState
 
-    if [ "$run_src_only" = true ]; then
-        echo -e "\nOnly running source"
+    if [[ "$run_src_only" = true ]]; then
+        log "Only running source"
         readSrc
     else
-        echo -e "\nRunning source and passing output to destination"
+        log "Running source and passing output to destination"
         sync
     fi
+}
+
+function fmtLog(){
+    if ((no_format)); then
+        fmtLog=""
+    else
+        fmtTime="[$(jq -r -n 'now|strflocaltime("%Y-%m-%d %T")')]"
+        if [ "$1" == "error" ]; then
+            fmtLog="$fmtTime ${RED}ERROR${NC} "
+        elif [ "$1" == "warn" ]; then
+            fmtLog="$fmtTime ${YELLOW}WARN${NC} "
+        else
+            fmtLog="$fmtTime ${BLUE}INFO${NC} "
+        fi
+    fi
+}
+
+function printLog() {
+    if jq -e . >/dev/null 2>&1 <<< "$1"; then
+        if ! ((no_format)); then
+            printf "$fmtLog \n"
+            echo "$*" | jq .
+        else
+            # Minify JSON
+            echo "$*" | jq -c .
+        fi
+    else
+        printf "$fmtLog"
+        printf "$* \n"
+    fi
+}
+
+function log() {
+    if ! ((silent)); then
+        fmtLog "info"
+        printLog "$*"
+    fi
+}
+
+function warn() {
+    if ! ((silent)); then
+        fmtLog "warn"
+        printLog "$*"
+    fi
+}
+
+function err() {
+    fmtLog "error"
+    printLog "$*"
+}
+
+function fail() {
+    err "Failed."
+    exit 1
 }
 
 main "$@"; exit
