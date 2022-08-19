88b1a75242aac4b527a89501d2f6ca1799ba4fa3
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-08-19 12:05:50 -0400

    FAI-2784 Add tests around src catalog + src check + dst config functionality (#34)

--- test/spec/airbyte-local_spec.sh
+++ test/spec/airbyte-local_spec.sh
@@ -58,4 +58,150 @@ Describe 'building source config'
     End
 End
 
+Describe 'building source catalog'
+    # Mock the docker command that invokes the Airbyte source "discover"
+    docker() {
+        if [[ $* =~ ^run.*discover ]]; then
+            echo '
+            {
+                "catalog": {
+                    "streams": [
+                    {
+                        "name": "faros_feed",
+                        "json_schema": {
+                        "$schema": "http://json-schema.org/draft-07/schema#",
+                        "type": "object",
+                        "properties": {
+                            "message": {
+                            "type": "string"
+                            }
+                        }
+                        },
+                        "supported_sync_modes": [
+                        "full_refresh",
+                        "incremental"
+                        ],
+                        "source_defined_cursor": true,
+                        "default_cursor_field": []
+                    }
+                    ]
+                },
+                "type": "CATALOG"
+            }'
+        fi
+    }
+
+    It 'uses discovered catalog'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --src-only \
+                --src.feed_cfg.feed_name 'jira-feed' \
+                --src.feed_cfg.feed_path 'tms/jira-feed' \
+                --debug
+        The output should include 'Using source configured catalog: {"streams":[{"stream":{"name":"faros_feed","json_schema":{}},"sync_mode":"incremental","destination_sync_mode":"append"}]}'
+    End
+    It 'full-refresh flag forces full refresh and overwrite mode'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --src-only \
+                --src.feed_cfg.feed_name 'jira-feed' \
+                --src.feed_cfg.feed_path 'tms/jira-feed' \
+                --full-refresh \
+                --debug
+        The output should include 'Using source configured catalog: {"streams":[{"stream":{"name":"faros_feed","json_schema":{}},"sync_mode":"full_refresh","destination_sync_mode":"overwrite"}]}'
+    End
+    It 'uses src-catalog-overrides sync mode'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --src-only \
+                --src.feed_cfg.feed_name 'jira-feed' \
+                --src.feed_cfg.feed_path 'tms/jira-feed' \
+                --src-catalog-overrides '{"faros_feed": {"sync_mode": "full_refresh"}}' \
+                --debug
+        The output should include 'Using source configured catalog: {"streams":[{"stream":{"name":"faros_feed","json_schema":{}},"sync_mode":"full_refresh","destination_sync_mode":"overwrite"}]}'
+    End
+    It 'ignores disabled streams'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --src-only \
+                --src.feed_cfg.feed_name 'jira-feed' \
+                --src.feed_cfg.feed_path 'tms/jira-feed' \
+                --src-catalog-overrides '{"faros_feed": {"disabled": true}}' \
+                --debug
+        The output should include 'Using source configured catalog: {"streams":[]}'
+    End
+End
+
+Describe 'check source'
+    It 'check successful'
+        # Mock the docker command that invokes the Airbyte source "check"
+        docker() {
+            if [[ $* =~ ^run.*check ]]; then
+                echo '
+                {"connectionStatus":{"status":"SUCCEEDED"},"type":"CONNECTION_STATUS"}'
+            fi
+        }
+
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --src-only \
+                --src.feed_cfg.feed_name 'jira-feed' \
+                --src.feed_cfg.feed_path 'tms/jira-feed' \
+                --check-connection
+
+        The output should include 'Connection validation successful'
+    End
+    It 'check failed'
+        # Mock the docker command that invokes the Airbyte source "check"
+        docker() {
+            if [[ $* =~ ^run.*check ]]; then
+                echo '
+                {"connectionStatus":{"status":"FAILED", "message":"Something went wrong"},"type":"CONNECTION_STATUS"}'
+            fi
+        }
+
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --src-only \
+                --src.feed_cfg.feed_name 'jira-feed' \
+                --src.feed_cfg.feed_path 'tms/jira-feed' \
+                --check-connection
+
+        The output should include 'Something went wrong'
+        The status should be failure
+    End
+End
+
+Describe 'building destination config'
+    # Makes the docker command a noop since we don't need it for these tests
+    docker() {
+        true
+    }
+
+    It 'merges keys recursively'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --dst 'farosai/dummy-destination-image' \
+                --dst.feed_cfg.inner_cfg.x '1' \
+                --dst.feed_cfg.inner_cfg.y '2' \
+                --dst.feed_cfg.inner_cfg.z '{"a":"3"}' \
+                --dst.feed_cfg.inner_cfg.z.b '4' \
+                --debug
+
+        The output should include 'Using destination config: {"feed_cfg":{"inner_cfg":{"y":2,"x":1,"z":{"a":"3","b":4}}}}'
+    End
+    It 'adds Faros SaaS specific config when using faros-destination'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --dst 'farosai/airbyte-faros-destination' \
+                --dst.faros_api_url 'http://faros' \
+                --dst.faros_api_key 'XYZ' \
+                --dst.graph 'g1' \
+                --dst-stream-prefix 'dummy_prefix' \
+                --debug
+
+        The output should include 'Using destination config: {"edition_configs":{"edition":"cloud","api_url":"http://faros","api_key":"XYZ","graph":"g1"}}'
+    End
+End
+
 
