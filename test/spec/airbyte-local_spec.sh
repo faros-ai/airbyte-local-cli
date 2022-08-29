5b40e9b10579c639d699a6a3a80bfe99a57c5587
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-08-29 13:43:57 -0400

    Rework destination cfg to support both Faros editions (#44)

--- test/spec/airbyte-local_spec.sh
+++ test/spec/airbyte-local_spec.sh
@@ -190,7 +190,7 @@ Describe 'building destination config'
 
         The output should include 'Using destination config: {"feed_cfg":{"inner_cfg":{"y":2,"x":1,"z":{"a":"3","b":4}}}}'
     End
-    It 'adds Faros SaaS specific config when using faros-destination'
+    It 'adds Faros SaaS specific config specified via special flags when using faros-destination'
         When run source ../airbyte-local.sh \
                 --src 'farosai/dummy-source-image' \
                 --dst 'farosai/airbyte-faros-destination' \
@@ -201,6 +201,30 @@ Describe 'building destination config'
                 --debug
 
         The output should include 'Using destination config: {"edition_configs":{"edition":"cloud","api_url":"http://faros","api_key":"XYZ","graph":"g1"}}'
+    End
+    It 'adds Faros SaaS specific config specified via special flags when using faros-destination while keeping other edition_configs'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --dst 'farosai/airbyte-faros-destination' \
+                --dst.faros_api_url 'http://faros' \
+                --dst.faros_api_key 'XYZ' \
+                --dst.graph 'g1' \
+                --dst.edition_configs.cloud_graphql_batch_size 10 \
+                --dst-stream-prefix 'dummy_prefix' \
+                --debug
+
+        The output should include 'Using destination config: {"edition_configs":{"cloud_graphql_batch_size":10,"edition":"cloud","api_url":"http://faros","api_key":"XYZ","graph":"g1"}}'
+    End
+    It 'adds Faros SaaS specific config specified via dst.* flags'
+        When run source ../airbyte-local.sh \
+                --src 'farosai/dummy-source-image' \
+                --dst 'farosai/airbyte-faros-destination' \
+                --dst.edition_configs '{"edition":"cloud","api_url":"http://faros","api_key":"XYZ","graph":"g1","cloud_graphql_batch_size":10}' \
+                --dst.edition_configs.check_connection 'true' \
+                --dst-stream-prefix 'dummy_prefix' \
+                --debug
+
+        The output should include 'Using destination config: {"edition_configs":{"edition":"cloud","api_url":"http://faros","api_key":"XYZ","graph":"g1","cloud_graphql_batch_size":10,"check_connection":true}}'
     End
 End
 
