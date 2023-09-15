ed1c4ecbd0c3f8e844a6e00c20b2fcd6be97ba09
Chris Wu <chris@faros.ai>
2023-09-14 18:56:28 -0700

    Error when unrecognized argument detected

--- airbyte-local.sh
+++ airbyte-local.sh
@@ -218,7 +218,7 @@ function parseFlags() {
             --help)
                 help ;;
             *)
-                err "Unrecognized arg: $1"
+                err "Unrecognized arg: $1" ;;
         esac
     done
 }
