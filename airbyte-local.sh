aa628683ff0bc89bdc20746b74cd35e8ea3ab584
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2023-09-22 10:35:24 -0400

    simplify jq expression

--- airbyte-local.sh
+++ airbyte-local.sh
@@ -321,7 +321,7 @@ function copyFarosConfig() {
     if [[ $src_docker_image == farosai/airbyte-faros-feeds-source* && $dst_docker_image == farosai/airbyte-faros-destination* ]]; then
             faros_config=$(jq '{faros: {api_url: .edition_configs.api_url, api_key: .edition_configs.api_key, graph: .edition_configs.graph, graphql_api: .edition_configs.graphql_api} | with_entries(if .value==null then empty else . end)}' "$tempdir/$dst_config_filename")
             debug "Updating source config with Faros API config from destination config: $(echo "$faros_config" | jq '.faros.api_key = "REDACTED"')"
-            jq --argjson faros_config "$faros_config" '. | $faros_config + .' "$tempdir/$src_config_filename" > "$tempdir/$src_config_filename.tmp"
+            jq --argjson faros_config "$faros_config" '$faros_config + .' "$tempdir/$src_config_filename" > "$tempdir/$src_config_filename.tmp"
             mv "$tempdir/$src_config_filename.tmp" "$tempdir/$src_config_filename"
             debug "Using source config: $(redactConfigSecrets "$(jq -c < $tempdir/$src_config_filename)" "$(specSrc)")"
     fi
