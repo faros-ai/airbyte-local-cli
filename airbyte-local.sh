bd0cd714701ba6e06f84ebd469c29942a64caefc
Matthew Tovbin <tovbinm@users.noreply.github.com>
2023-11-29 20:31:55 -0800

    Compact json rendering

--- airbyte-local.sh
+++ airbyte-local.sh
@@ -321,7 +321,7 @@ function copyFarosConfig() {
     if [[ $src_docker_image == farosai/airbyte-faros-feeds-source* && $dst_docker_image == farosai/airbyte-faros-destination* ]]; then
             # Extract Faros API config from destination config
             faros_config=$(jq '{faros: {api_url: .edition_configs.api_url, api_key: .edition_configs.api_key, graph: .edition_configs.graph, graphql_api: .edition_configs.graphql_api} | with_entries(if .value==null then empty else . end)}' "$tempdir/$dst_config_filename")
-            debug "Updating source config with Faros API config from destination config: $(echo "$faros_config" | jq '.faros.api_key = "REDACTED"')"
+            debug "Updating source config with Faros API config from destination config: $(echo "$faros_config" | jq -c '.faros.api_key = "REDACTED"')"
             # Merge Faros API config into source config
             jq --argjson faros_config "$faros_config" '$faros_config + .' "$tempdir/$src_config_filename" > "$tempdir/$src_config_filename.tmp"
             mv "$tempdir/$src_config_filename.tmp" "$tempdir/$src_config_filename"
