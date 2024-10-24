c76fa8d30cd1dbfcb5e1d85030a824a882f2ac37
Jennie Gao <jennie@faros.ai>
2024-10-23 17:20:56 -0700

    add config file

--- airbyte_local_cli_node/sample_command.sh
+++ airbyte_local_cli_node/sample_command.sh
@@ -4,6 +4,31 @@ node ./lib/index.js \
   --dst 'farosai/airbyte-faros-destination' \
   --state state.json \
   --check-connection
+node ./lib/index.js \
+  --src 'farosai/airbyte-servicenow-source' \
+  --src.username '<source_username>' \
+  --src.password '<source_password>' \
+  --src.url '<source_url>' \
+  --dst 'farosai/airbyte-faros-destination' \
+  --dst.edition_configs.edition 'cloud' \
+  --dst.edition_configs.api_url '<faros_api_url>' \
+  --dst.edition_configs.api_key '<faros_api_key>' \
+  --dst.edition_configs.graph 'default' \
+  --state state.json \
+  --check-connection
+node ./lib/index.js \
+  --src 'farosai/airbyte-servicenow-source' \
+  --src.username '<source_username>' \
+  --src.password '<source_password>' \
+  --src.url '<source_url>' \
+  --dst 'farosai/airbyte-faros-destination' \
+  --dst.edition_configs.edition 'cloud' \
+  --dst.edition_configs.api_url '<faros_api_url>' \
+  --dst.edition_configs.api_key '<faros_api_key>' \
+  --dst.edition_configs.graph 'default' \
+  --state state.json \
+  --check-connection \
+  --output-config 'out/jg-config.json'
 
 vercel-pkg/index-macos --help
 vercel-pkg/index-macos \
@@ -15,6 +40,19 @@ vercel-pkg/index-macos \
 ./out/yao-pkg/index-macos  \
   --src 'farosai/airbyte-servicenow-source' \
   --dst 'farosai/airbyte-faros-destination'
+./out/yao-pkg/index-macos  \
+  --src 'farosai/airbyte-servicenow-source' \
+  --src.username '<source_username>' \
+  --src.password '<source_password>' \
+  --src.url '<source_url>' \
+  --dst 'farosai/airbyte-faros-destination' \
+  --dst.edition_configs.edition 'cloud' \
+  --dst.edition_configs.api_url '<faros_api_url>' \
+  --dst.edition_configs.api_key '<faros_api_key>' \
+  --dst.edition_configs.graph 'default' \
+  --state state.json \
+  --check-connection \
+  --output-config 'out/jg-config.json'
 
 ./out/sea/test-cli --help
 ./out/sea/test-cli \
--- airbyte_local_cli_node/src/index.ts
+++ airbyte_local_cli_node/src/index.ts
@@ -1,13 +1,22 @@
+import {writeFileSync} from 'node:fs';
+import {join} from 'node:path';
+
 import {Command} from 'commander';
 import Docker from 'dockerode';
 
 const program = new Command();
 const docker = new Docker();
+let config: any = {};
 
 program
   .name('airbyte-local.sh')
   .description('Airbyte local CLI')
   .version('1.0.0')
+  .allowUnknownOption(true) // TODO: Handle unknown options with errors
+  // for testing purposes
+  .option('--test-docker', 'Test spinning up a docker container')
+  // for customizing output config file location
+  .option('--output-config <path>', 'The location to output the config to a file')
   .requiredOption('--src <image>', 'Airbyte source Docker image')
   .requiredOption('--dst <image>', 'Airbyte destination Docker image')
   .option('--src.<key> <value>', 'Add "key": "value" into the source config')
@@ -38,10 +47,53 @@ program
   .option('--max-cpus <cpus>', 'Set maximum CPUs each Docker or Kubernetes container can use')
   .option('--src-docker-options <string>', 'Set additional options to pass to the "docker run <src>" command')
   .option('--dst-docker-options <string>', 'Set additional options to pass to the "docker run <dst>" command')
-  .option('--debug', 'Enable debug logging');
+  .option('--debug', 'Enable debug logging')
+  .action((options) => {
+    const configKeyValues = parseKeyValue();
+    options.srcConfig = configKeyValues.srcConfig;
+    options.dstConfig = configKeyValues.dstConfig;
+    config = options;
+    console.log('Options:', options);
+  });
+
+/**
+ * Parse the key-value pair
+ * TODO: Parse out the nested key-value pairs
+ */
+function parseKeyValue() {
+  const args = process.argv;
+  const srcConfig: Record<string, string> = {};
+  const dstConfig: Record<string, string> = {};
+
+  args.forEach((arg, index) => {
+    if (arg.startsWith('--src.')) {
+      const key = arg.split('.')[1];
+      const value = args[index + 1];
+      if (key && value && !value.startsWith('--')) {
+        srcConfig[key] = value;
+      }
+    } else if (arg.startsWith('--dst.')) {
+      const key = arg.split('.')[1];
+      const value = args[index + 1];
+      if (key && value && !value.startsWith('--')) {
+        dstConfig[key] = value;
+      }
+    }
+  });
+  console.log('Source Config:', srcConfig);
+  console.log('Destination Config:', dstConfig);
+  return {srcConfig, dstConfig};
+}
+
+/** Write the config to a json file */
+function writeConfig() {
+  const customPath = config.outputConfig ?? 'out/test_config.json';
+  const file = join(process.cwd(), customPath);
+  writeFileSync(file, JSON.stringify(config, null, 2));
+}
 
 /** Spin up a test docker container */
-async function runDocker() {
+async function runTestContainer() {
   const src_docker_image = 'farosai/airbyte-faros-feeds-source';
   try {
     const container = await docker.createContainer({
@@ -51,22 +103,28 @@ async function runDocker() {
     });
     console.info('Creating the docker container...');
     await container.start();
+    console.info('Docker container is running...');
     await container.wait();
     console.info('Docker container is finished.');
-    // await container.remove();
+    await container.remove();
+    console.info('Docker container is removed.');
   } catch (error: any) {
     console.error(`Error spinning up the docker: ${error.message}`);
   }
 }
 
 async function main() {
-  // echo the command line arguments
-  program.parse(process.argv);
-  const options = program.opts();
-  console.log('Options:', options);
+  // parse the command line arguments
+  program.parse();
+
+  // write config to a json file
+  writeConfig();
 
   // spin up a test docker container
-  await runDocker();
+  const options = program.opts();
+  if (options['testDocker']) {
+    await runTestContainer();
+  }
 }
 
 main().catch((error) => {
