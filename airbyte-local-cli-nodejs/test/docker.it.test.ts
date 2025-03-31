449ccb2a7676368e36359c4b21c1a7d271f00f56
Jennie Gao <jennie@faros.ai>
2025-03-31 12:17:46 -0500

    [FAI-15334] Pipe source output directly to destination connector (#125)

--- airbyte-local-cli-nodejs/test/docker.it.test.ts
+++ airbyte-local-cli-nodejs/test/docker.it.test.ts
@@ -1,5 +1,13 @@
-import {copyFileSync, readFileSync, rmSync, unlinkSync, writeFileSync} from 'node:fs';
-import {Writable} from 'node:stream';
+import {
+  copyFileSync,
+  createReadStream,
+  createWriteStream,
+  readFileSync,
+  rmSync,
+  unlinkSync,
+  writeFileSync,
+} from 'node:fs';
+import {PassThrough, Writable} from 'node:stream';
 
 import {
   checkSrcConnection,
@@ -13,6 +21,8 @@ import {
 import {FarosConfig} from '../src/types';
 import {
   DST_CONFIG_FILENAME,
+  OutputStream,
+  setupStreams,
   SRC_OUTPUT_DATA_FILE,
   TMP_SPEC_CONFIG_FILENAME,
   TMP_WIZARD_CONFIG_FILENAME,
@@ -94,7 +104,11 @@ describe('runSrcSync', () => {
   });
 
   it('should success', async () => {
-    await expect(runSrcSync(testTmpDir, testCfg)).resolves.not.toThrow();
+    const {passThrough, srcOutputStream} = setupStreams();
+    const writeStream = createWriteStream(`${testTmpDir}/${SRC_OUTPUT_DATA_FILE}`);
+    passThrough.pipe(writeStream);
+
+    await expect(runSrcSync(testTmpDir, testCfg, srcOutputStream)).resolves.not.toThrow();
 
     // Replace timestamp and version for snapshot comparison
     const output = readFileSync(`${testTmpDir}/${SRC_OUTPUT_DATA_FILE}`, 'utf8');
@@ -107,7 +121,33 @@ describe('runSrcSync', () => {
     expect(outputWithoutTS.join('\n')).toMatchSnapshot();
   });
 
-  // Check stderr message is correctly redirect to process.stderr
+  it('should success with specified output file', async () => {
+    await expect(
+      runSrcSync(testTmpDir, {...testCfg, srcOutputFile: `${testTmpDir}/${SRC_OUTPUT_DATA_FILE}`}),
+    ).resolves.not.toThrow();
+
+    // Replace timestamp and version for snapshot comparison
+    const output = readFileSync(`${testTmpDir}/${SRC_OUTPUT_DATA_FILE}`, 'utf8');
+    const outputWithoutTS = output.split('\n').map((line) => {
+      return line
+        .replace(/"timestamp":\d+/g, '"timestamp":***')
+        .replace(/"sourceVersion":"[\w.-]+"/g, '"sourceVersion":***')
+        .replace(/Source version: [\w.-]+/g, 'Source version: ***');
+    });
+    expect(outputWithoutTS.join('\n')).toMatchSnapshot();
+  });
+
+  it('should success with stdout', async () => {
+    // print out to logger info
+    await expect(
+      runSrcSync(testTmpDir, {
+        ...testCfg,
+        srcOutputFile: OutputStream.STDOUT,
+      }),
+    ).resolves.not.toThrow();
+  });
+
+  // Check stdout message is correctly redirect to process.stderr
   it('should fail', async () => {
     // Capture process.stderr
     let stderrData = '';
@@ -175,7 +215,30 @@ describe('runDstSync', () => {
     dstConfig.edition_configs.api_key = process.env['FAROS_API_KEY'];
     writeFileSync(dstConfigPath, JSON.stringify(dstConfig, null, 2));
 
-    await expect(runDstSync(testTmpDir, testCfg)).resolves.not.toThrow();
+    const passThrough = new PassThrough();
+    const readStream = createReadStream(`${testTmpDir}/${SRC_OUTPUT_DATA_FILE}`);
+    readStream.pipe(passThrough);
+
+    await expect(runDstSync(testTmpDir, testCfg, passThrough)).resolves.not.toThrow();
+
+    const stateData = readFileSync(testStateFile, 'utf8');
+    expect(JSON.parse(stateData).data).toBeTruthy();
+    expect(stateData).toMatchSnapshot();
+  }, 60000);
+
+  it('should success with srcInputFile', async () => {
+    // check if the API key is provided
+    expect((process.env['FAROS_API_KEY'] ?? '').length > 0);
+    const dstConfig = JSON.parse(readFileSync(dstConfigPathTemplate, 'utf8'));
+    dstConfig.edition_configs.api_key = process.env['FAROS_API_KEY'];
+    writeFileSync(dstConfigPath, JSON.stringify(dstConfig, null, 2));
+
+    await expect(
+      runDstSync(testTmpDir, {
+        ...testCfg,
+        srcInputFile: `${testTmpDir}/${SRC_OUTPUT_DATA_FILE}`,
+      }),
+    ).resolves.not.toThrow();
 
     const stateData = readFileSync(testStateFile, 'utf8');
     expect(JSON.parse(stateData).data).toBeTruthy();
