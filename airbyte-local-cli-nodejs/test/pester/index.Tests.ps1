d4058e3b1c88c9abe63abe678f939f840eee089a
Jennie Gao <jennie@faros.ai>
2025-04-11 16:50:44 -0700

    update to do matching for multiple lines

--- airbyte-local-cli-nodejs/test/pester/index.Tests.ps1
+++ airbyte-local-cli-nodejs/test/pester/index.Tests.ps1
@@ -5,37 +5,43 @@ Describe 'Cli argv validation' {
     # Option conflict failures
     It 'fails if using both --config-file and --src' {
         $result = & ./airbyte-local --config-file 'some_test_path' --src 'farosai/airbyte-servicenow-source'
-        $result | Should -Match "option '-c, --config-file <path>' cannot be used with option '--src <image>'"
+        $matchingLine = $result | Where-Object { $_ -match "option '-c, --config-file <path>' cannot be used with option '--src <image>'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'fails if using both --config-file and --dst' {
         $result = & ./airbyte-local --config-file 'some_test_path' --dst 'farosai/airbyte-faros-destination'
-        $result | Should -Match "option '-c, --config-file <path>' cannot be used with option '--dst <image>'"
+        $matchingLine = $result | Where-Object { $_ -match "option '-c, --config-file <path>' cannot be used with option '--dst <image>'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'fails if using both --src-only and --src-output-file' {
         $result = & ./airbyte-local --src-output-file 'some_test_path' --src-only
-        $result | Should -Match "option '--src-only' cannot be used with option '--src-output-file <path>'"
+        $matchingLine = $result | Where-Object { $_ -match "option '--src-only' cannot be used with option '--src-output-file <path>'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'fails if using both --src-only and --dst-only' {
         $result = & ./airbyte-local --src-only --dst-only 'some_test_path'
-        $result | Should -Match "option '--dst-only <path>' cannot be used with option '--src-only'"
+        $matchingLine = $result | Where-Object { $_ -match "option '--dst-only <path>' cannot be used with option '--src-only'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'fails if using both --src-output-file and --dst-only' {
         $result = & ./airbyte-local --src-output-file 'some_test_path' --dst-only 'some_test_path'
-        $result | Should -Match "option '--dst-only <path>' cannot be used with option '--src-output-file <path>'"
+        $matchingLine = $result | Where-Object { $_ -match "option '--dst-only <path>' cannot be used with option '--src-output-file <path>'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'fails if using both --src-check-connection and --dst-only' {
         $result = & ./airbyte-local --src-check-connection --dst-only 'some_test_path'
-        $result | Should -Match "option '--dst-only <path>' cannot be used with option '--src-check-connection'"
+        $matchingLine = $result | Where-Object { $_ -match "option '--dst-only <path>' cannot be used with option '--src-check-connection'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 }
@@ -43,13 +49,15 @@ Describe 'Cli argv validation' {
 Describe 'Validate temporary directory and files creation' {
     It 'check docker in shellspec test environment' {
         $result = & docker --version
-        $result | Should -Match "Docker version"
+        $matchingLine = $result | Where-Object { $_ -match "Docker version" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 0
     }
 
     It 'should create temporary directory' {
         $result = & ./airbyte-local --src 'farosai/foo' --dst 'farosai/bar' --debug
-        $result | Should -Match "Temporary directory created"
+        $matchingLine = $result | Where-Object { $_ -match "Temporary directory created" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 }
@@ -57,16 +65,21 @@ Describe 'Validate temporary directory and files creation' {
 Describe 'Stream prefix' {
     It 'should generate stream prefix' {
         $result = & ./airbyte-local --src 'farosai/airbyte-foo-source' --dst 'farosai/airbyte-faros-destination-bar' --debug
-        $result | Should -Match "Using connection name: myfoosrc"
-        $result | Should -Match "Using destination stream prefix: myfoosrc__foo__"
+        $matchingLine = $result | Where-Object { $_ -match "Using connection name: myfoosrc" }
+        $matchingLine | Should -Not -BeNullOrEmpty
+        $matchingLine = $result | Where-Object { $_ -match "Using destination stream prefix: myfoosrc__foo__" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'should generate stream prefix with connection name' {
         $result = & ./airbyte-local --src 'farosai/airbyte-foo-source' --dst 'farosai/airbyte-faros-destination-bar' --connection-name 'jennie-connection' --debug
-        $result | Should -Match "Using connection name: jennie-connection"
-        $result | Should -Match "Using destination stream prefix: jennie-connection__foo__"
-        $result | Should -Match "State file 'jennie-connection__state.json' not found. An empty state file will be created."
+        $matchingLine = $result | Where-Object { $_ -match "Using connection name: jennie-connection" }
+        $matchingLine | Should -Not -BeNullOrEmpty
+        $matchingLine = $result | Where-Object { $_ -match "Using destination stream prefix: jennie-connection__foo__" }
+        $matchingLine | Should -Not -BeNullOrEmpty
+        $matchingLine = $result | Where-Object { $_ -match "State file 'jennie-connection__state.json' not found. An empty state file will be created." }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 }
@@ -74,19 +87,22 @@ Describe 'Stream prefix' {
 Describe 'Load state file' {
     It 'should create state file' {
         $result = & ./airbyte-local --src 'farosai/airbyte-foo-source' --dst 'farosai/airbyte-faros-destination-bar' --debug
-        $result | Should -Match "State file 'myfoosrc__state.json' not found. An empty state file will be created."
+        $matchingLine = $result | Where-Object { $_ -match "State file 'myfoosrc__state.json' not found. An empty state file will be created." }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'should load state file' {
         $result = & ./airbyte-local --src 'farosai/foo' --dst 'farosai/bar' --state-file './resources/test__state.json'
-        $result | Should -Match "Using state file: './resources/test__state.json'"
+        $matchingLine = $result | Where-Object { $_ -match "Using state file: './resources/test__state.json'" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 
     It 'should fail if provided state file path is invalid' {
         $result = & ./airbyte-local --src 'farosai/foo' --dst 'farosai/bar' --state-file 'invalid_file'
-        $result | Should -Match "State file 'invalid_file' not found. Please make sure the state file exists and have read access."
+        $matchingLine = $result | Where-Object { $_ -match "State file 'invalid_file' not found. Please make sure the state file exists and have read access." }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 }
@@ -94,7 +110,8 @@ Describe 'Load state file' {
 Describe 'No image pull' {
     It 'should not pull images' {
         $result = & ./airbyte-local --src 'farosai/foo' --dst 'farosai/bar' --no-src-pull --no-dst-pull
-        $result | Should -Not -Match "Pulling docker image"
+        $matchingLine = $result | Where-Object { $_ -notmatch "Pulling docker image" }
+        $matchingLine | Should -Not -BeNullOrEmpty
         $LASTEXITCODE | Should -Be 1
     }
 }
