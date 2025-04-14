ad3b06aeec524bf22444c88611cae37d9a411fce
Jennie Gao <jennie@faros.ai>
2025-04-14 14:58:10 -0700

    update shellspec to use tag

--- airbyte-local-cli-nodejs/test/exec/spec/index_spec.sh
+++ airbyte-local-cli-nodejs/test/exec/spec/index_spec.sh
@@ -1,6 +1,15 @@
-# Run tests that don't involve docker containers
+injectApiKey() {
+  jq --arg api_key "$FAROS_API_KEY" '
+        .dst.config.edition_configs.api_key = $api_key
+      ' ./resources/test_config_file_dst_only.json.template > ./resources/test_config_file_dst_only.json
+  jq --arg api_key "$FAROS_API_KEY" '
+    .src.config.api_key = $api_key |
+    .dst.config.edition_configs.api_key = $api_key
+  ' ./resources/test_config_file_graph_copy.json.template > ./resources/test_config_file_graph_copy.json
+}
+BeforeAll 'injectApiKey'
 
-Describe 'Cli argv validation'
+Describe 'Cli argv validation' 'argvParsing'
   # Option conflict failures
   It 'fails if using both --config-file and --src'
     airbyte_local_test() {
@@ -124,7 +133,7 @@ Describe 'Cli argv validation'
   End
 End
 
-Describe 'Validate temporary directory and files creation'
+Describe 'Validate temporary directory and files creation' 'main'
   It 'check docker in shellspec test environment'
     When run docker --version
     The output should include "Docker version"
@@ -144,7 +153,7 @@ Describe 'Validate temporary directory and files creation'
   End
 End
 
-Describe 'Stream prefix'
+Describe 'Stream prefix' 'main'
   It 'should generate stream prefix'
     airbyte_local_test() {
       ./airbyte-local \
@@ -175,7 +184,7 @@ Describe 'Stream prefix'
   End
 End
 
-Describe 'Load state file'
+Describe 'Load state file' 'main'
   It 'should create state file'
     airbyte_local_test() {
       ./airbyte-local \
@@ -213,7 +222,7 @@ Describe 'Load state file'
   End
 End
 
-Describe 'No image pull'
+Describe 'No image pull' 'main'
   It 'should not pull images'
     airbyte_local_test() {
       ./airbyte-local \
@@ -228,11 +237,163 @@ Describe 'No image pull'
   End
 End
 
+Describe 'Generate config' 'docker'
+  It 'should succeed'
+    airbyte_local_test() {
+      ./airbyte-local generate-config faros-graphql
+    }
+    When call airbyte_local_test
+    The output should include "Configuration file generated successfully"
+    The status should equal 0
+  End
+  It 'should succeed with static config'
+    airbyte_local_test() {
+      ./airbyte-local generate-config github
+    }
+    When call airbyte_local_test
+    The output should include "Configuration file generated successfully"
+    The status should equal 0
+  End
+  It 'should succeed with slient option'
+    airbyte_local_test() {
+      ./airbyte-local generate-config --silent github
+    }
+    When call airbyte_local_test
+    The output should not include "Source Airbyte Configuration Spec"
+    The output should include "Configuration file generated successfully"
+    The status should equal 0
+  End
+  It 'should succeed with custom image'
+    airbyte_local_test() {
+      ./airbyte-local generate-config --image farosai/airbyte-faros-graphql-source
+    }
+    When call airbyte_local_test
+    The output should include "Configuration file generated successfully"
+    The status should equal 0
+  End
+End
+
+Describe 'Check source connection' 'docker'
+  It 'should fail if source connection fails'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --src 'farosai/airbyte-example-source' \
+        --src-check-connection \
+        --src-only
+    }
+    When call airbyte_local_test
+    The output should include "Failed to validate source connection: User is not chris."
+    The status should equal 1
+  End
+  It 'should fail if source connection fails'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --config-file './resources/test_config_file_src_only.json' \
+        --src-check-connection \
+        --src-only
+    }
+    When call airbyte_local_test
+    The output should include "Source connection is valid."
+    The status should equal 0
+  End
+End
+
+Describe 'Run source sync only' 'docker'
+  It 'should fail if source sync fails auth'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --src 'farosai/airbyte-faros-graphql-source' \
+        --src-only
+    }
+    When call airbyte_local_test
+    The stderr should include "Faros API key was not provided"
+    The output should include "Failed to discover catalog"
+    The status should equal 1
+  End
+  It 'should succeed with srcOnly'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --config-file './resources/test_config_file_src_only.json' \
+        --src-only
+    }
+    When call airbyte_local_test
+    The status should equal 0
+    The output should include "Source connector completed."
+  End
+  It 'should succeed with srcOnly and output file'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --config-file './resources/test_config_file_src_only.json' \
+        --src-output-file 'test_src_output_file'
+
+      grep -q '"uid":"5"' test_src_output_file
+    }
+    When call airbyte_local_test
+    The output should include "Source connector completed."
+    The status should equal 0
+  End
+End
+
+Describe 'Run destination sync' 'docker'
+  It 'should succeed with dstOnly'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --config-file './resources/test_config_file_dst_only.json' \
+        --dst-only './resources/dockerIt_runDstSync/faros_airbyte_cli_src_output' \
+        --debug
+    }
+    When call airbyte_local_test
+    The output should include '[DST] - {"log":{"level":"INFO","message":"Errored 0 records"},"type":"LOG"}'
+    The output should include "Destination connector completed."
+    The output should include "Airbyte CLI completed."
+    The status should equal 0
+  End
+End
+
+Describe 'Run source and destination sync' 'docker'
+  It 'should succeed with src and dst'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --config-file './resources/test_config_file_graph_copy.json'
+    }
+    When call airbyte_local_test
+    The output should include "Source connector completed."
+
+    # default is incremental sync
+    The output should include '[SRC] - {"log":{"level":"INFO","message":"Catalog: {\"streams\":[{\"stream\":{\"name\":\"faros_graph\",\"json_schema\":{},\"supported_sync_modes\":[\"full_refresh\",\"incremental\"]},\"sync_mode\":\"incremental\",\"destination_sync_mode\":\"append\"}]}"},"type":"LOG"}'
+    The output should include '[DST] - {"log":{"level":"INFO","message":"Catalog: {\"streams\":[{\"stream\":{\"name\":\"myfarosgraphqlsrc__faros_graphql__faros_graph\",\"json_schema\":{},\"supported_sync_modes\":[\"full_refresh\",\"incremental\"]},\"sync_mode\":\"incremental\",\"destination_sync_mode\":\"append\"}]}"},"type":"LOG"}'
+
+    The output should include '[DST] - {"log":{"level":"INFO","message":"Errored 0 records"},"type":"LOG"}'
+    The output should include "Destination connector completed."
+    The output should include "Airbyte CLI completed."
+    The status should equal 0
+  End
+  It 'should succeed with full refresh'
+    airbyte_local_test() {
+      ./airbyte-local \
+        --config-file './resources/test_config_file_graph_copy.json' \
+        --full-refresh
+    }
+    When call airbyte_local_test
+    The output should include "Source connector completed."
+    
+    # catalog should be full refresh
+    The output should include '[SRC] - {"log":{"level":"INFO","message":"Catalog: {\"streams\":[{\"stream\":{\"name\":\"faros_graph\",\"json_schema\":{},\"supported_sync_modes\":[\"full_refresh\",\"incremental\"]},\"sync_mode\":\"full_refresh\",\"destination_sync_mode\":\"overwrite\"}]}"},"type":"LOG"}'
+    The output should include '[DST] - {"log":{"level":"INFO","message":"Catalog: {\"streams\":[{\"stream\":{\"name\":\"myfarosgraphqlsrc__faros_graphql__faros_graph\",\"json_schema\":{},\"supported_sync_modes\":[\"full_refresh\",\"incremental\"]},\"sync_mode\":\"full_refresh\",\"destination_sync_mode\":\"overwrite\"}]}"},"type":"LOG"}'
+
+    The output should include "Destination connector completed."
+    The output should include "Airbyte CLI completed."
+    The status should equal 0
+  End
+End
+
 # Clean up temeporary test files
 cleanup() {
   find . -maxdepth 1 -name 'faros_airbyte_cli_config.json' -delete
   find . -maxdepth 1 -name 'test_src_output_file' -delete
   find . -maxdepth 1 -name '*state.json' -delete
+  find ./resources/ -name 'test_config_file_dst_only.json' -delete
+  find ./resources/ -name 'test_config_file_graph_copy.json' -delete
 
 }
 AfterAll 'cleanup'
